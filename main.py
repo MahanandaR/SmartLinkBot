74efe6c053da90b22ec22588c9aa289115cf8536
MahanandaR <sonymr555@gmail.com>
2025-06-29 12:57:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from rag import generate_answer, process_urls
 import sys
 import pysqlite3
-sys.modules["sqlite3"] = pysqlite3
+sys.modules["sqlite3"]=pysqlite3
 
 # Page config
 st.set_page_config(page_title="Smart URL Answer Bot", page_icon="🔗", layout="centered")
